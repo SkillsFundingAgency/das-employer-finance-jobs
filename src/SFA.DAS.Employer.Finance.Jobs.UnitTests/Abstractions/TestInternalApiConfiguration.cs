6be310c6bb733307fd7bec3a2e6c4677c940603a
SARWAR, Muhammed <Muhammed.SARWAR@EDUCATION.GOV.UK>
2025-12-09 15:57:59 +0000

    fix for naming issue

--- src/SFA.DAS.Employer.Finance.Jobs.UnitTests/Abstractions/TestInternalApiConfiguration.cs
+++ src/SFA.DAS.Employer.Finance.Jobs.UnitTests/Abstractions/TestInternalApiConfiguration.cs
@@ -1,10 +1,8 @@
 ﻿using SFA.DAS.Employer.Finance.Jobs.Infrastructure.SharedApi.Interfaces;
 
-namespace SFA.DAS.Employer.Finance.Jobs.UnitTests.Abstractions
+namespace SFA.DAS.Employer.Finance.Jobs.UnitTests.Abstractions;
+public class TestInternalApiConfiguration : IInternalApiConfiguration
 {
-    public class TestInternalApiConfiguration : IInternalApiConfiguration
-    {
-        public virtual string Url { get; set; }
-        public virtual string Identifier { get; set; }
-    }
+    public virtual string Url { get; set; }
+    public virtual string Identifier { get; set; }
 }
