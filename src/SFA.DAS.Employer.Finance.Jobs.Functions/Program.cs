65265e0778c9872ff9c256b63c2568c37b4579dc
SARWAR, Muhammed <Muhammed.SARWAR@EDUCATION.GOV.UK>
2026-01-14 11:49:55 +0000

    adding NServicebus config

--- src/SFA.DAS.Employer.Finance.Jobs.Functions/Program.cs
+++ src/SFA.DAS.Employer.Finance.Jobs.Functions/Program.cs
@@ -17,24 +17,22 @@ var host = new HostBuilder()
          services.AddServiceRegistration(configuration);
          services.AddApplicationInsightsTelemetryWorkerService();
          services.ConfigureFunctionsApplicationInsights();
+
+         services
+       .AddLogging(builder =>
+       {
+           builder.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
+           builder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Information);
+
+           builder.AddFilter(typeof(Program).Namespace, LogLevel.Information);
+           builder.SetMinimumLevel(LogLevel.Trace);
+           builder.AddConsole();
+
+       });
      })
-    .UseNServiceBus()
-
-    .ConfigureServices((context, services) =>
-    {
-        services
-        .AddLogging(builder =>
-        {
-            builder.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
-            builder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Information);
-
-            builder.AddFilter(typeof(Program).Namespace, LogLevel.Information);
-            builder.SetMinimumLevel(LogLevel.Trace);
-            builder.AddConsole();
-
-        })
-            .AddApplicationInsightsTelemetryWorkerService()
-            .ConfigureFunctionsApplicationInsights();
-    })
+    .UseNServiceBus((config, endpointConfiguration) =>
+     {
+          endpointConfiguration.AdvancedConfiguration.EnableInstallers();
+     })    
     .Build();
-await host.RunAsync();
+     await host.RunAsync();
