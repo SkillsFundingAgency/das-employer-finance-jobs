af4b8839e6c65ae6bc32fe9564964491dfb1556f
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2026-02-19 12:24:31 +0000

    startup logging

--- src/SFA.DAS.Employer.Finance.Jobs.Functions.ProcessAccountPayments/Program.cs
+++ src/SFA.DAS.Employer.Finance.Jobs.Functions.ProcessAccountPayments/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.DurableTask.Client;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.Employer.Finance.Jobs.Infrastructure.Extensions;
 using SFA.DAS.Employer.Finance.Jobs.ProcessAccountPaymentsFunction.Orchestrators;
 
 [assembly: NServiceBusTriggerFunction("SFA.DAS.Employer.Finance.Jobs.PAP")]
+
+Console.WriteLine("=== ProcessAccountPayments Function App Starting ===");
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
@@ -22,5 +25,10 @@ var host = new HostBuilder()
         services.AddServiceRegistration(configuration);
     })    
     .Build();
+
+Console.WriteLine("=== ProcessAccountPayments Function App Built Successfully ===");
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+logger.LogInformation("ProcessAccountPayments Function App starting up...");
+
 await host.RunAsync();
 
