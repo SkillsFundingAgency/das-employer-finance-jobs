72f3eabd273c883bf26ef05ab0e7279e14428315
SARWAR, Muhammed <Muhammed.SARWAR@EDUCATION.GOV.UK>
2026-03-12 10:42:36 +0000

    truning of low level trace for testing

--- src/SFA.DAS.Employer.Finance.Jobs.Infrastructure/Extensions/LoggingExtensions.cs
+++ src/SFA.DAS.Employer.Finance.Jobs.Infrastructure/Extensions/LoggingExtensions.cs
@@ -11,19 +11,19 @@ public static class LoggingExtensions
         services.AddLogging(builder =>
         {
             builder.AddApplicationInsights();
-            // Application Insights filters (following das-recruit-jobs pattern)
-            builder.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
-            builder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Information);
-            builder.AddFilter<ApplicationInsightsLoggerProvider>("SFA.DAS", LogLevel.Information);
+            //// Application Insights filters (following das-recruit-jobs pattern)
+            //builder.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
+            //builder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Information);
+            //builder.AddFilter<ApplicationInsightsLoggerProvider>("SFA.DAS", LogLevel.Information);
             
-            // General filters
-            builder.AddFilter("Microsoft", LogLevel.Warning);
-            builder.AddFilter("System", LogLevel.Warning);
-            builder.AddFilter("NServiceBus", LogLevel.Debug);
-            builder.AddFilter("StartupDiagnostics", LogLevel.Information);
-            builder.AddFilter("SFA.DAS", LogLevel.Information);
+            //// General filters
+            //builder.AddFilter("Microsoft", LogLevel.Warning);
+            //builder.AddFilter("System", LogLevel.Warning);
+            //builder.AddFilter("NServiceBus", LogLevel.Debug);
+            //builder.AddFilter("StartupDiagnostics", LogLevel.Information);
+            //builder.AddFilter("SFA.DAS", LogLevel.Information);
 
-            builder.SetMinimumLevel(LogLevel.Trace);
+            //builder.SetMinimumLevel(LogLevel.Trace);
             builder.AddConsole();
         });
 
