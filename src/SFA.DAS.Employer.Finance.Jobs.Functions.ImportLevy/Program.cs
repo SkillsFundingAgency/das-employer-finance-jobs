420109d10be805439a151a7c8d7ab4483554f028
AHMED, Sumair <Sumair.AHMED@EDUCATION.GOV.UK>
2026-04-08 14:39:42 +0100

    pr comments

--- src/SFA.DAS.Employer.Finance.Jobs.Functions.ImportLevy/Program.cs
+++ src/SFA.DAS.Employer.Finance.Jobs.Functions.ImportLevy/Program.cs
@@ -1,16 +1,19 @@
 using Microsoft.Extensions.Hosting;
-using SFA.DAS.Employer.Finance.Jobs.Functions.ImportLevy.Extensions;
+using InfrastructureConfigurationExtensions = SFA.DAS.Employer.Finance.Jobs.Infrastructure.Extensions.ConfigurationExtensions;
+using InfrastructureConfigurationOptionsExtension = SFA.DAS.Employer.Finance.Jobs.Infrastructure.Extensions.AddConfigurationOptionsExtension;
+using InfrastructureServiceRegistrationExtensions = SFA.DAS.Employer.Finance.Jobs.Infrastructure.Extensions.ServiceRegistrationExtensions;
+using ImportLevyLoggingExtensions = SFA.DAS.Employer.Finance.Jobs.Functions.ImportLevy.Extensions.LoggingExtensions;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .ConfigureAppConfiguration(builder => SFA.DAS.Employer.Finance.Jobs.Infrastructure.Extensions.ConfigurationExtensions.BuildDasConfiguration(builder))
+    .ConfigureAppConfiguration(builder => InfrastructureConfigurationExtensions.BuildDasConfiguration(builder))
     .ConfigureServices((context, services) =>
     {
         var configuration = context.Configuration;
 
-        services.AddDasLogging();
-        SFA.DAS.Employer.Finance.Jobs.Infrastructure.Extensions.AddConfigurationOptionsExtension.AddConfigurationOptions(services, configuration);
-        SFA.DAS.Employer.Finance.Jobs.Infrastructure.Extensions.ServiceRegistrationExtensions.AddServiceRegistration(services, configuration);
+        ImportLevyLoggingExtensions.AddDasLogging(services);
+        InfrastructureConfigurationOptionsExtension.AddConfigurationOptions(services, configuration);
+        InfrastructureServiceRegistrationExtensions.AddServiceRegistration(services, configuration);
     })
     .Build();
 
