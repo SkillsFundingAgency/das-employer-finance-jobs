8ec3ca5367ae1ad2a7507a98b45f20b5f7ab141c
AHMED, Sumair <Sumair.AHMED@EDUCATION.GOV.UK>
2026-04-28 17:39:00 +0100

    wip before dependency

--- src/SFA.DAS.Employer.Finance.Jobs.Functions.ImportLevy/Program.cs
+++ src/SFA.DAS.Employer.Finance.Jobs.Functions.ImportLevy/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SFA.DAS.Employer.Finance.Jobs.Functions.ImportLevy.Extensions;
+using SFA.DAS.Employer.Finance.Jobs.Functions.ImportLevy.Services;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
@@ -11,6 +13,7 @@ var host = new HostBuilder()
         services.AddDasLogging();
         SFA.DAS.Employer.Finance.Jobs.Infrastructure.Extensions.AddConfigurationOptionsExtension.AddConfigurationOptions(services, configuration);
         SFA.DAS.Employer.Finance.Jobs.Infrastructure.Extensions.ServiceRegistrationExtensions.AddServiceRegistration(services, configuration);
+        services.AddScoped<ILevyDeclarationNormalizer, LevyDeclarationNormalizer>();
     })
     .Build();
 
