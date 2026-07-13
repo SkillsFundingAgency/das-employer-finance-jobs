3e15b40f1dac570ad30c6ca1bf2928cc0325ad94
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2026-07-13 13:23:18 +0100

    add logging config

--- src/SFA.DAS.Employer.Finance.Jobs.Infrastructure/Extensions/LoggingExtensions.cs
+++ src/SFA.DAS.Employer.Finance.Jobs.Infrastructure/Extensions/LoggingExtensions.cs
@@ -10,13 +10,24 @@ public static class LoggingExtensions
     {
         services.AddLogging(builder =>
         {
-            // Application Insights filters (following das-recruit-jobs pattern)
+            // Application Insights: keep business logs, suppress Azure SDK / durable framework noise.
             builder.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
-            builder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Information);
+            builder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Warning);
+            builder.AddFilter<ApplicationInsightsLoggerProvider>("Azure", LogLevel.Warning);
+            builder.AddFilter<ApplicationInsightsLoggerProvider>("Azure.Core", LogLevel.Warning);
+            builder.AddFilter<ApplicationInsightsLoggerProvider>("Azure.Storage", LogLevel.Warning);
+            builder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft.Azure.WebJobs", LogLevel.Warning);
+            builder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft.DurableTask", LogLevel.Warning);
+            builder.AddFilter<ApplicationInsightsLoggerProvider>("DurableTask", LogLevel.Warning);
             builder.AddFilter<ApplicationInsightsLoggerProvider>("SFA.DAS", LogLevel.Information);
 
             // General filters
             builder.AddFilter("Microsoft", LogLevel.Warning);
+            builder.AddFilter("Azure", LogLevel.Warning);
+            builder.AddFilter("Azure.Core", LogLevel.Warning);
+            builder.AddFilter("Azure.Storage", LogLevel.Warning);
+            builder.AddFilter("Microsoft.Azure.WebJobs", LogLevel.Warning);
+            builder.AddFilter("Microsoft.DurableTask", LogLevel.Warning);
             builder.AddFilter("System", LogLevel.Warning);
             builder.AddFilter("StartupDiagnostics", LogLevel.Information);
             builder.AddFilter("SFA.DAS", LogLevel.Information);
