458cf6a7cabb77b33390e318df351690f213560b
AHMED, Sumair <Sumair.AHMED@EDUCATION.GOV.UK>
2026-08-07 13:24:15 +0100

    Remove duplicate HMRC throttle implementation

--- src/SFA.DAS.Employer.Finance.Jobs.Infrastructure/Interfaces/IHmrcRequestThrottle.cs
+++
@@ -1,6 +0,0 @@
-namespace SFA.DAS.Employer.Finance.Jobs.Infrastructure.Interfaces;
-
-public interface IHmrcRequestThrottle
-{
-    Task WaitAsync(string operationName, CancellationToken cancellationToken = default);
-}
