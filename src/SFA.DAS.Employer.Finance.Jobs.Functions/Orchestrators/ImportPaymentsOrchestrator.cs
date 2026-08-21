513b8d236a8864d2812ec9ea851a5ead8653b436
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2026-08-21 09:11:26 +0100

    remove filtering to single account

--- src/SFA.DAS.Employer.Finance.Jobs.Functions/Orchestrators/ImportPaymentsOrchestrator.cs
+++ src/SFA.DAS.Employer.Finance.Jobs.Functions/Orchestrators/ImportPaymentsOrchestrator.cs
@@ -17,17 +17,8 @@ public class ImportPaymentsOrchestrator(ILogger<ImportPaymentsOrchestrator> logg
         var correlationId = input?.CorrelationId ?? context.NewGuid().ToString();
         var maxConcurrentAccounts = ImportPaymentsOptions.GetMaxConcurrentAccountsOrDefault(input?.MaxConcurrentAccounts);
         var maxConcurrentPeriodEnds = ImportPaymentsOptions.GetMaxConcurrentPeriodEndsOrDefault(input?.MaxConcurrentPeriodEnds);
-        var targetAccountId = input?.TargetAccountId;
 
         logger.LogInformation("[CorrelationId: {CorrelationId}] ImportPaymentsOrchestrator started", correlationId);
-
-        if (targetAccountId.HasValue)
-        {
-            logger.LogInformation(
-                "[CorrelationId: {CorrelationId}] ImportPaymentsOrchestrator is restricted to AccountId {TargetAccountId}",
-                correlationId,
-                targetAccountId.Value);
-        }
 
         var result = new ImportPaymentsResult
         {
@@ -49,7 +40,7 @@ public class ImportPaymentsOrchestrator(ILogger<ImportPaymentsOrchestrator> logg
                 result.CreatedPeriodEndsCount = createdPeriodEnds.Count;
                 result.FailedPeriodEndsCount = newPeriodEnds.Count - createdPeriodEnds.Count;
 
-                await ProcessPeriodEndAccountsInParallel(context, createdPeriodEnds, correlationId, maxConcurrentAccounts, maxConcurrentPeriodEnds, targetAccountId);
+                await ProcessPeriodEndAccountsInParallel(context, createdPeriodEnds, correlationId, maxConcurrentAccounts, maxConcurrentPeriodEnds);
             }
             else
             {
@@ -128,8 +119,7 @@ public class ImportPaymentsOrchestrator(ILogger<ImportPaymentsOrchestrator> logg
         IReadOnlyCollection<PeriodEnd> periodEnds,
         string correlationId,
         int maxConcurrentAccounts,
-        int maxConcurrentPeriodEnds,
-        long? targetAccountId)
+        int maxConcurrentPeriodEnds)
     {
         var activePeriodEndTasks = new List<Task<PeriodEndResult>>();
         var periodEndsScheduled = 0;
@@ -151,8 +141,7 @@ public class ImportPaymentsOrchestrator(ILogger<ImportPaymentsOrchestrator> logg
                 {
                     CorrelationId = correlationId,
                     PeriodEnd = periodEnd,
-                    MaxConcurrentAccounts = maxConcurrentAccounts,
-                    TargetAccountId = targetAccountId
+                    MaxConcurrentAccounts = maxConcurrentAccounts
                 },
                 new SubOrchestrationOptions { InstanceId = instanceId }));
 
